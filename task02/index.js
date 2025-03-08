e1d2bce833f0f7838190076ab857355ecf581bae
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-08 14:50:14 +0200

    Add To-Dos API endpoints

--- task02/index.js
+++ task02/index.js
@@ -2,3 +2,41 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const port = 3000;
+
+app.use(express.json())
+
+// Implement the following API endpoints: 
+// ○  POST /todos - Create a new todo (default status: "in-progress") 
+// ○  GET /todos - List all todos 
+// ○  GET /todos/:id - Get a single todo by ID 
+// ○  PUT /todos/:id - Update a todo (modify title or status) 
+// ○  DELETE /todos/:id - Delete a todo 
+
+
+app.get('/',(req,res)=>{
+  res.send("<h1>Welcome to our todo list app!</h1>")
+})
+
+app.post('/todos',(req,res)=>{
+
+})
+
+app.get('/todos',(req,res)=>{
+res.json({"hello":"true"})
+})
+
+app.get('/todos/:id',(req,res)=>{
+
+})
+
+app.put('/todos/:id',(req,res)=>{
+
+})
+
+app.delete('/todos/:id',(req,res)=>{
+
+})
+
+app.listen(port,()=>{
+  console.log(`Server running on port ${port}`)
+})
