a4a10158ab2a544db291d24f29905846ae80d364
Mohamed Sofy <mediumsofy@gmail.com>
2025-03-09 00:01:05 +0200

    Implement updating a to-do item

--- task02/index.js
+++ task02/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // ○  PUT /todos/:id - Update a todo (modify title or status)
 // ○  DELETE /todos/:id - Delete a todo
 
-const writeToDo = (todo) => {
+const createToDo = (todo) => {
   const dataString = fs.readFileSync("./db.json", { encoding: "utf-8" });
   const data = JSON.parse(dataString);
   data.push(todo);
@@ -20,17 +20,34 @@ const writeToDo = (todo) => {
   // console.log(data);
 };
 
-// writeToDo({ id: 2, title: "Get Groceries", status: "In-progress" });
-
-const readToDos = () => {
+const indexToDo = () => {
   const data = fs.readFileSync("./db.json", { encoding: "utf-8" });
-  return JSON.parse(data)
+  return JSON.parse(data);
 };
 
-const updateToDo = (id, data) => {
 
+const updateToDo = (id, todo) => {
+  // read data from file as a string
+  const dataString = fs.readFileSync("./db.json", { encoding: "utf-8" });
+  // convert to an object to traverse & manipulate
+  const data = JSON.parse(dataString);
+  console.log("The data before updating: ", data);
+  // loop through data array and compare ids
+  // till you find the desired item
+  const item = data.filter((item) => item.id == id)[0]; // the filter() returns the result as an array that contains a single object,
+  // with the [0] we are targeting that object
+
+  // update values
+  item.title = todo.title ?? item.title;
+  item.status = todo.status ?? item.status;
+  data[id - 1] = item;
+  console.log("the item is: ", item);
+  console.log("the data after updating: ", data);
+  // write to disk
+  fs.writeFileSync("./db.json", JSON.stringify(data));
 };
 
+// updateToDo(2, { title: "modified", status: "done" });
 const deleteToDo = (id) => {};
 
 app.get("/", (req, res) => {
@@ -39,21 +56,27 @@ app.get("/", (req, res) => {
 
 // POST /todos - Create a new todo (default status: "in-progress")
 app.post("/todos", (req, res) => {
-  writeToDo(req.body)
-  console.log('ToDo posted successfully!')
+  createToDo(req.body);
+  console.log("ToDo posted successfully!");
 });
 
 // GET /todos - List all todos
 app.get("/todos", (req, res) => {
-  const data = readToDos()
-  res.send(data)
+  const data = indexToDo();
+  res.send(data);
 });
 
 // GET /todos/:id - Get a single todo by ID
 app.get("/todos/:id", (req, res) => {});
 
 // PUT /todos/:id - Update a todo (modify title or status)
-app.put("/todos/:id", (req, res) => {});
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(req.body);
+  updateToDo(id, req.body);
+  const data = readToDos();
+  res.send(data);
+});
 
 // DELETE /todos/:id - Delete a todo
 app.delete("/todos/:id", (req, res) => {});
